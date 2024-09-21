f2c41b67cb98d594e1a03f7d766e0096875af10e
shubham sarode <shubham.sarode@extwebtech.in>
2024-09-21 23:21:15 +0530

    created the app tsx file for routing

--- public/images/assets.ts
+++ public/images/assets.ts
@@ -1,3 +1,4 @@
 export const healthcareLogo = require("./healthcareLogo.svg")
 export const bgBannerImg = require("./backGroundBanner.svg")
 export const profileImg = require("./profileImage.svg")
+export const videoIconImg = require("./videoIcon.svg")
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,30 +1,34 @@
-import { profileImg } from '@/public/images/assets'
+import { profileImg, videoIconImg } from '@/public/images/assets'
 import Image from 'next/image'
 import React from 'react'
 
 function Banner() {
     return (
-        <section>
-            <div className="container mx-auto">
+        <section className='w-full'>
+            <div className="w-full  md:container  mx-auto px-4">
                 <div className="grid grid-cols-12 gap-4 justify-center">
-                    <div className="col-span-6 ">
-                        <h2 className="text-2xl">
-                            Providing Quality Healthcare for a Brighter and Healthy Future
+                    <div className="col-span-12 md:col-span-6  flex flex-col justify-center items-center ">
+                        <h2 className="text-[40px] font-lato font-semibold text-black-300">
+                            Providing Quality <span className='text-primary'>Healthcare</span> for a <span className='text-secondary'>Brighter</span> and <span className='text-secondary'>Healthy</span> Future
                         </h2>
-                        <p className="sm">At our hospital, we are dedicated to providing exceptional medical care to our patients and their families. Our experienced team of medical professionals, cutting-edge technology, and compassionate approach make us a leader in the healthcare industry</p>
-                        <div className="flex justify-between">
-                            <button className="button">
+                        <p className="sm my-5 font-lato font-normal text-xl text-ifno">At our hospital, we are dedicated to providing exceptional medical care to our patients and their families. Our experienced team of medical professionals, cutting-edge technology, and compassionate approach make us a leader in the healthcare industry</p>
+                        <div className="flex mt-10 justify-between items-center w-full flex-wrap gap-2">
+                            <button className="button !bg-primary !text-white hover:!bg-transparent hover:!text-primary ">
                                 Appointment
                             </button>
-                            <button className="button">
-                                Watch video
+                            
+                            <button className="button flex justify-between items-center gap-4 ">
+                            <div className="p-3 bg-primary rounded-[50%] grid justify-center items-center">
+                            <Image src={videoIconImg} alt='video icon'  /> 
+                            </div>
+                               
+                               <span > Watch video</span>
                             </button>
                         </div>
                     </div>
-                    <div className="col-span-6 ">
-                        <div className="banner-box-2">
-                            <Image src={profileImg} className='h-screen object-cover' alt='profile image doctor app' />
-
+                    <div className="col-span-12 md:col-span-6  flex justify-center items-center m-auto">
+                        <div className="banner-box-2 ">
+                            <Image src={profileImg} className=' object-cover' alt='profile image doctor app' />
                         </div>
 
                     </div>
