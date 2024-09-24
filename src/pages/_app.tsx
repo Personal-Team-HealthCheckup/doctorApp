cb9c009f32b5e4ad317b1c5c1eb78bd3b5aecfcd
shubham sarode <shubham.sarode@extwebtech.in>
2024-09-24 20:44:46 +0530

    removed unnecessary spaces

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import Header from "../components/Header";
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
+import Header from "../components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
