6213729d7185d4e5f58f13d1c7aa37a47ba098bb
shubham sarode <shubham.sarode@extwebtech.in>
2024-09-24 10:38:00 +0530

    setup test cases

---
+++ __tests__/Page.test.tsx
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Page from '../src/pages/index'
+ 
+describe('Page', () => {
+  it('renders a heading', () => {
+    render(<Page />)
+    expect(screen).toMatchSnapshot()
+  })
+})
